75fea46411b01257db63e20eb39dec2e9e8e3291
Sontus Chandra Anik <info.sontus@gmail.com>
2022-01-19 15:03:14 +0600

    Login & Registration Module Done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\UsersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+// Backend Controllers 
+
+Route::resource('users',UsersController::class);
